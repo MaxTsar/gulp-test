d591b54dd0a1dacd792dbe306131959873e2602d
MaxTsar <rep0trett88@gmail.com>
2017-11-19 22:39:42 +0300

    less add on branch2

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 const   gulp    = require('gulp'),
+        less    = require('gulp-less'),
         babel   = require('gulp-babel');
 
 gulp.task('js', function() {
     gulp.src('./src/js/*.js')
         .pipe(babel())
         .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('less', function() {
+    gulp.src('./src/less/*.less')
+        .pipe(less())
+        .pipe(gulp.dest('./build/css'))
 });
 
